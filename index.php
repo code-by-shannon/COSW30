519422364aa471b188e6b60cb99bef4576562be7
Shannon <sestumpf@gmail.com>
2026-02-12 16:40:43 -0800

    week 1 work finished

--- index.php
+++ index.php
@@ -7,3 +7,24 @@ echo print_r($colors);
 echo '</pre>';
 
 ?>
+
+<?php
+$batman_universe = array('Batman', 'Catwoman', 'The Joker', 'Commissioner Gordon');
+
+echo "<h2>"
+     . $batman_universe[0] 
+     . ' and ' 
+     . $batman_universe[3] 
+     . ' teamed up and defeated ' 
+     . $batman_universe[1] 
+     . ' and ' 
+     . $batman_universe[2]
+     . '!'
+     . "</h2>";
+?>
+
+<?php
+
+$newArr = array('Ritchie', 'Jon', 'Ian', 'Ian', 'Roger');
+echo print_r($newArr);
+?>
